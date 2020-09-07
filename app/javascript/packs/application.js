be392dd58fc31af29e46ee0bb7c0d3db77da780d
Ademir Ferreira <whoisademir@gmail.com>
2020-09-07 14:23:35 -0300

    Add template configuration

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,9 +9,5 @@ require("@rails/activestorage").start()
 require("channels")
 
 
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+import bulmaQuickview from 'bulma-quickview/src/js'
+document.addEventListener('turbolinks:load', () => { let quickviews = bulmaQuickview.attach()})
